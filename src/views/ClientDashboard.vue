079fecadc92205345b023cd4b0051d0979d70599
Agustin <agustin.jimenez@clara.team>
2024-01-25 14:00:04 -0300

    adding remaining files

--- src/views/ClientDashboard.vue
+++ src/views/ClientDashboard.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="dashboard">
     <!-- your components -->
+    <v-card class="mx-auto" max-width="344" variant="outlined">
+      <v-card-item>
+        <div>
+          <div class="text-overline mb-1">OVERLINE</div>
+          <div class="text-h6 mb-1">Headline</div>
+          <div class="text-caption">
+            Greyhound divisely hello coldly fonwderfully
+          </div>
+        </div>
+      </v-card-item>
+
+      <v-card-actions>
+        <v-btn variant="outlined"> Button </v-btn>
+      </v-card-actions>
+    </v-card>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue"
+import { defineComponent } from "vue";
 export default defineComponent({
-  name: 'ClientDashboard',
+  name: "ClientDashboard",
   setup() {
     // your code
-  }
-})
+  },
+});
 </script>
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
--- vue.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  devServer: {
-    host: '0.0.0.0'
-  },
-  pluginOptions: {
-    mock: { entry: './mock/index.js', debug: true },
-  },
-}
